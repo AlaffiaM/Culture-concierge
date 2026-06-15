542e8b70449566de51968c46ef16a4bb99e66b0b
Agbaje Stephen <agbajestephen5@gmail.com>
2026-06-15 17:35:54 +0100

    Update verifyToken.js

--- Backend/verifyToken.js
+++ Backend/verifyToken.js
@@ -1,3 +1,4 @@
 const jwt = require('jsonwebtoken')
 
 const PROJECT_ID = 'alaffia-concierge'
+const CERTS_URL = 'https://www.googleapis.com/robot/v1/metadata/x509/securetoken@system.gserviceaccount.com'
