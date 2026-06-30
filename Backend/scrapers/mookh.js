317c46e678527c05e494627dde2a8129b420ff24
Agbaje Stephen <agbajestephen5@gmail.com>
2026-06-30 19:55:07 +0100

    Update mookh.js

--- Backend/scrapers/mookh.js
+++ Backend/scrapers/mookh.js
@@ -64,7 +64,7 @@ function extractCity(text) {
 async function scrape() {
   const browser = await puppeteer.launch({
     headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage'],
   })
 
   const events = []
